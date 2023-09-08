ee74507efc2bb84de6dfb83fb6900a450caa1dfd
Triadi M <ahmadtriadi265@gmail.com>
2023-09-08 14:56:36 +0800

    fix: popup widget desktop

--- lib/components/popup_menu_desktop.dart
+++ lib/components/popup_menu_desktop.dart
@@ -1,41 +1,19 @@
+import 'package:deepfake/components/custom_button_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:deepfake/utility.dart';
 
-class AlertDesktop extends StatelessWidget {
-  const AlertDesktop({super.key});
+class PopupMenuWidgetDesktop extends StatelessWidget {
+  const PopupMenuWidgetDesktop({super.key, required this.contents});
+
+  final List<Widget> contents;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 200, top: 134, right: 200),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    "assets/exclamation.png",
-                    width: 34,
-                  ),
-                  SizedBox(
-                    width: 6,
-                  ),
-                  Text(
-                    "Warning",
-                    style: desktopH3.copyWith(color: neutral700),
-                  ),
-                  const SizedBox(
-                    height: 23,
-                  ),
-                  Text(
-                      "This is a call to action for users and creators alike. As users, we\nmust exercise discernment, fact-check sources, and question the\n veracity of content before sharing. Responsible creators, on the\nother hand, must uphold ethical standards and refrain from using\n deepfakes for malicious purposes.")
-                ],
-              ),
-            ],
-          ),
-        ),
+     return AlertDialogDesktop(
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: contents,
       ),
     );
   }
